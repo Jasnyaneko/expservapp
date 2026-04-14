a7c5ed8cafe0fd410ba103ab8c818dbff2a19594
jasnyaneko <jasper.on.the.net@gmail.com>
2026-04-14 13:30:30 +0200

    petit flag si email correct ou pas

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const form = document.querySelector("#iciform")
+const mail = document.querySelector("#icimail")
+
+
+mail.addEventListener("input",(event)=>{
+    console.log("key")
+    let validemail = null
+    username = mail.value
+    console.log(username.length)
+    console.log(username.lastIndexOf(".")
+    )
+    if( username.lastIndexOf(".") > username.lastIndexOf("@") + 1 && username.lastIndexOf(".") != username.length - 2 && username.lastIndexOf("@") != 0){
+        mail.style.color= "green" ;  validemail = true }
+    else{mail.style.color = "red" ; validemail = false}
+})
+
 
 form.addEventListener("submit",(event)=>{
     event.preventDefault() ;
     const formdat = new FormData(form)
      username = formdat.get("username")
-     console.log(username)
-        atid = username.lastIndexOf("@")
-        console.log(atid)
-        pointid = username.lastIndexOf(".")
-        console.log(pointid)
-        console.log(username.length)
-        if (pointid > atid + 1 && pointid < username.length){ let formobject = {}
+        if (username.lastIndexOf(".") > username.lastIndexOf("@") + 1 && username.lastIndexOf(".") < username.length + 1){
+             let formobject = {}
     formdat.forEach((value,keys)=>{
         formobject[keys] = value
     })
     recup(formobject)
     }
 else{console.log("wrong")}}) ;
+
 
 async function recup (json){
     try{
